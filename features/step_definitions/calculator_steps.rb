ecd1a2d22138ae427b728518a9341ad91d125349
zhangxu <xuzhang@thoughtworks.com>
2015-05-04 22:57:59 +0800

    add step definition file

---
+++ features/step_definitions/calculator_steps.rb
@@ -0,0 +1,11 @@
+Given /^the input "([^"]*)"$/ do |arg1|
+  pending
+end
+
+Given /^the calculator is run$/ do
+  pending
+end
+
+Then /^the output should be "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
